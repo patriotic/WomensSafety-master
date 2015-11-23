83a00fa9a6c695ca6bc2bcf6dbc3a58933fec905
Syed Ikhtiar Ahmed <ikhtiar0240@gmail.com>
2015-11-23 04:26:45 -0800

    updated nav bar+map 02

--- app/src/main/java/com/creative/womensafety/MapsActivity.java
+++ app/src/main/java/com/creative/womensafety/MapsActivity.java
@@ -7,6 +7,8 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -20,6 +22,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -92,6 +95,8 @@ public class MapsActivity extends FragmentActivity {
 
 
 
+        setUpVictimInfo();
+
         sendRequestToServer(AppConstant.DirectionApiUrl(gps.getLatitude(), gps.getLongitude(), lattitude, langitude));
 
         mMap.addMarker(new MarkerOptions().position(position).title(getLocationDetails(lattitude,langitude)));
@@ -100,9 +105,26 @@ public class MapsActivity extends FragmentActivity {
 
 
     }
+    private void setUpVictimInfo() {
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            public View getInfoWindow(Marker arg0) {
+                View v = getLayoutInflater().inflate(R.layout.custom_infowindow, null);
+                TextView tv = (TextView) v.findViewById(R.id.victimInfo);
+                tv.setText(getLocationDetails(lattitude, langitude));
+                return v;
+            }
+
+            public View getInfoContents(Marker arg0) {
+
+                return null;
+
+            }
+        });
+    }
     private String getLocationDetails(double lat,double lang) {
         Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-        String add="VICTIM ";
+        String add="        VICTIM        ";
 
         List<Address> addresses  = null;
         try {
@@ -112,22 +134,38 @@ public class MapsActivity extends FragmentActivity {
         }
 
         if(addresses!=null) {
-            String address = addresses.get(0).getAddressLine(0);
+            String address="";
+            for(int i=0;i<addresses.get(0).getMaxAddressLineIndex();i++){
+                address += addresses.get(0).getAddressLine(i);
+                if(addresses.get(0).getAddressLine(i+1)!=null && !addresses.get(0).getAddressLine(i+1).isEmpty())
+                    address +="\n";
+            }
+
             String knownName = addresses.get(0).getFeatureName();
             String premises = addresses.get(0).getPremises();
             String city = addresses.get(0).getLocality();
             String state = addresses.get(0).getAdminArea();
             String zip = addresses.get(0).getPostalCode();
             String country = addresses.get(0).getCountryName();
+            Bundle extras = addresses.get(0).getExtras();
+            String subadminarea = addresses.get(0).getSubAdminArea();
+            String SubLocality  = addresses.get(0).getSubLocality();
+            String SubThoroughfare = addresses.get(0).getSubThoroughfare();
+            String Thoroughfare = addresses.get(0).getThoroughfare();
 
 
-            if(address!=null && !address.isEmpty())add+=","+address;
-//            if(knownName!=null && !knownName.isEmpty())add+=","+knownName;
-//            if(premises!=null && !premises.isEmpty())add+=","+premises;
-            if(zip!=null && !zip.isEmpty())add+=","+zip;
-            if(state!=null && !state.isEmpty())add+=","+state;
-            if(city!=null && !city.isEmpty())add+=","+city;
-            if(country!=null && !country.isEmpty())add+=","+country;
+            if(address!=null && !address.isEmpty())add+="\n\nAddressLine : "+address;
+            if(knownName!=null && !knownName.isEmpty())add+="\nFeatureName : "+knownName;
+            if(premises!=null && !premises.isEmpty())add+="\nPremises : "+premises;
+            if(zip!=null && !zip.isEmpty())add+="\nZip : "+zip;
+            if(state!=null && !state.isEmpty())add+="\nState : "+state;
+            if(city!=null && !city.isEmpty())add+="\nCity : "+city;
+            if(country!=null && !country.isEmpty())add+="\nCountry : "+country;
+            if(extras!=null && !extras.isEmpty())add+="\nExtras : "+extras;
+            if(subadminarea!=null && !subadminarea.isEmpty())add+="\nSubAdminArea : "+subadminarea;
+            if(SubLocality !=null && !SubLocality.isEmpty())add+="\nSubLocality : "+SubLocality;
+            if(SubThoroughfare !=null && !SubThoroughfare.isEmpty())add+="\nSubThoroughfare : "+SubThoroughfare;
+            if(Thoroughfare !=null && !Thoroughfare.isEmpty())add+="\nThoroughfare : "+Thoroughfare;
         }
         return add;
     }
